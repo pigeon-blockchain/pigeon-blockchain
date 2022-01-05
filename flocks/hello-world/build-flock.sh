8f0011217da4586b54530d25b4572a67ba859d31
Joseph C Wang <joe@pigeonchain.co>
2022-01-05 20:52:34 +0800

    add curl to /tmp

--- flocks/hello-world/build-flock.sh
+++ flocks/hello-world/build-flock.sh
@@ -5,16 +5,14 @@ set -v
 
 # Create a container
 container=$(buildah from fedora:35)
+mountpoint=$(buildah mount $container)
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Labels are part of the "buildah config" command
 buildah config --label maintainer="Joseph C Wang <joe@pigeonchain.co>" $container
 
 # Grab the source code outside of the container
-if [ ! -e hello-2.10.tar.gz ] ; then
-curl -sSL http://ftpmirror.gnu.org/hello/hello-2.10.tar.gz -o hello-2.10.tar.gz
-fi
-
-buildah copy $container hello-2.10.tar.gz /tmp/hello-2.10.tar.gz
+curl -sSL http://ftpmirror.gnu.org/hello/hello-2.10.tar.gz -o $mountpoint/tmp/hello-2.10.tar.gz
 
 buildah run $container dnf install -y tar gzip gcc make
 buildah run $container dnf clean all
