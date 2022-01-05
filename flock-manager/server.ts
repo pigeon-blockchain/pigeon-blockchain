03282c1161295bd2c069f8d615bcfb07f6cf8dd0
Joseph C Wang <joe@pigeonchain.co>
2022-01-05 11:11:57 +0800

    change to typescript

--- flock-manager/server.ts
+++ flock-manager/server.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-const zmq = require("zeromq")
-const { encode, decode } = require("@msgpack/msgpack");
+import zmq = require("zeromq")
+import { encode, decode } from "@msgpack/msgpack";
 
 async function run() {
   const sock = new zmq.Reply
   await sock.bind("tcp://127.0.0.1:3000")
 
   for await (const [msg] of sock) {
-    const object = parseInt(decode(msg));
+    const object = parseInt(decode(msg).toString());
     await sock.send(encode(2 * object))
   }
 }
