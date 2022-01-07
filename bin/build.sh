7bad25f1653e104720bdfddc83796d42806873a8
Joseph C Wang <joe@pigeonchain.co>
2022-01-07 13:08:54 +0800

    move hello-world to c

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR > /dev/null
-pushd ../flocks/hello-world > /dev/null
+pushd ../flocks/examples/c > /dev/null
 npm run build
 popd > /dev/null
 pushd ../flocks/pigeon-blockchain > /dev/null
