f4cef10ee66756460cf94b8dd75ef18c471c9eb3
Joseph C Wang <joe@pigeonchain.co>
2022-01-12 18:33:01 +0800

    fix lints

--- flocks/examples/nodejs/example-nodejs-flock/server.ts
+++ flocks/examples/nodejs/example-nodejs-flock/server.ts
@@ -2,17 +2,15 @@
 import zmq = require("zeromq")
 import { encode, decode } from "@msgpack/msgpack";
 import util = require('util')
-const execShPromise = require("exec-sh").promise;
-var pod : string = "";
 
 async function run() {
-  const sock = new zmq.Reply
+  const sock = new zmq.Reply()
   await sock.bind("tcp://127.0.0.1:3000")
 
   for await (const [msg] of sock) {
     const inobj: any = decode(msg);
     const data: any = decode(inobj.data);
-    const retval = 2 * parseInt(data.toString());
+    const retval = 2 * parseInt(data.toString(), 10);
     await sock.send(encode(retval))
   }
 }
