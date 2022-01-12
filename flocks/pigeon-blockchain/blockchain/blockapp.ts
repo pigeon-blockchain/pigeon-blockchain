09e6d6f831040cae1fa82218eb3e739392ee57eb
Joseph C Wang <joe@pigeonchain.co>
2022-01-12 18:42:10 +0800

    add lint

--- flocks/pigeon-blockchain/blockchain/blockapp.ts
+++ flocks/pigeon-blockchain/blockchain/blockapp.ts
@@ -1,38 +1,38 @@
 #!/usr/bin/env node
-import zmq = require("zeromq")
-import { encode, decode } from "@msgpack/msgpack";
+import zmq = require('zeromq')
+import { encode, decode } from '@msgpack/msgpack'
 import 'regenerator-runtime/runtime'
 import blockchain = require('vanilla-blockchain')
 
-async function run_reply(
+async function runReply (
   blockchain : any,
   sock : zmq.Reply,
-  pub_sock : zmq.Publisher
+  pubSock : zmq.Publisher
 ) : Promise<void> {
   for await (const [msg] of sock) {
-    const inobj: any = decode(msg);
-    var retval;
-    if (inobj.cmd == "echo") {
-      retval = inobj.data;
-    } else if (inobj.cmd == "blockchain") {
-      const { hash: previousHash } = blockchain.latestBlock;
-      retval = await blockchain.addBlock( inobj.data, previousHash );
-      pub_sock.send(encode(retval))
+    const inobj: any = decode(msg)
+    let retval
+    if (inobj.cmd === 'echo') {
+      retval = inobj.data
+    } else if (inobj.cmd === 'blockchain') {
+      const { hash: previousHash } = blockchain.latestBlock
+      retval = await blockchain.addBlock(inobj.data, previousHash)
+      pubSock.send(encode(retval))
     } else {
-      retval = "unknown command";
+      retval = 'unknown command'
     }
     await sock.send(encode(retval))
   }
 }
 
-async function main() : Promise<void> {
-  const reply_sock = new zmq.Reply
-  const pub_sock = new zmq.Publisher
-  await reply_sock.bind("tcp://127.0.0.1:3000")
-  await pub_sock.bind("tcp://127.0.0.1:3001")
-  const bc = await new blockchain.AsyncBlockchain();
+async function main () : Promise<void> {
+  const replySock = new zmq.Reply()
+  const pubSock = new zmq.Publisher()
+  await replySock.bind('tcp://127.0.0.1:3000')
+  await pubSock.bind('tcp://127.0.0.1:3001')
+  const bc = await new blockchain.AsyncBlockchain()
   console.log('starting blockchain')
-  run_reply(bc, reply_sock, pub_sock)
+  runReply(bc, replySock, pubSock)
 }
 
 main()
