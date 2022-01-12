7abf68f2ad501da9e721f532f135dad1c686fa0d
Joseph C Wang <joe@pigeonchain.co>
2022-01-12 18:45:31 +0800

    add lint

--- flocks/examples/nodejs/example-nodejs-flock/server.ts
+++ flocks/examples/nodejs/example-nodejs-flock/server.ts
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
-import zmq = require("zeromq")
-import { encode, decode } from "@msgpack/msgpack";
-import util = require('util')
+import zmq = require('zeromq')
+import { encode, decode } from '@msgpack/msgpack'
 
-async function run() {
+async function run () {
   const sock = new zmq.Reply()
-  await sock.bind("tcp://127.0.0.1:3000")
+  await sock.bind('tcp://127.0.0.1:3000')
 
   for await (const [msg] of sock) {
-    const inobj: any = decode(msg);
-    const data: any = decode(inobj.data);
-    const retval = 2 * parseInt(data.toString(), 10);
+    const inobj: any = decode(msg)
+    const data: any = decode(inobj.data)
+    const retval = 2 * parseInt(data.toString(), 10)
     await sock.send(encode(retval))
   }
 }
