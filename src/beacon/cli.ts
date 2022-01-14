7daeb26416a2a05440780fe059b97f56391213df
Joseph C Wang <joe@pigeonchain.co>
2022-01-14 20:54:59 +0800

    fix lint

--- src/beacon/cli.ts
+++ src/beacon/cli.ts
@@ -1,36 +1,36 @@
 #!/usr/bin/env node
-import zmq = require("zeromq")
+import zmq = require('zeromq')
 import readline = require('readline');
-import { encode, decode } from "@msgpack/msgpack";
+import { encode, decode } from '@msgpack/msgpack'
 
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-function my_split(
+function mySplit (
   string: string,
   delimiter: string,
   n: number
 ) {
-    var parts = string.split(delimiter);
-    return parts.slice(0, n - 1).concat([parts.slice(n - 1).join(delimiter)]);
+  const parts = string.split(delimiter)
+  return parts.slice(0, n - 1).concat([parts.slice(n - 1).join(delimiter)])
 }
 
-async function run() {
-    const sock = new zmq.Request
-    sock.connect("tcp://127.0.0.1:3000")
-    console.log("Producer bound to port 3000")
-    const asyncReadline = function() {
-      rl.question("Command: ", async function(answer) {
-	const [cmd, data] = my_split(answer, " ", 2);
-	sock.send(encode({"cmd": cmd, "data": data}))
-	const [result] = await sock.receive()
-	console.log(decode(result));
-	asyncReadline();
-      });
-    }
-    asyncReadline();
+async function run () {
+  const sock = new zmq.Request()
+  sock.connect('tcp://127.0.0.1:3000')
+  console.log('Producer bound to port 3000')
+  const asyncReadline = function () {
+    rl.question('Command: ', async function (answer) {
+      const [cmd, data] = mySplit(answer, ' ', 2)
+      sock.send(encode({ cmd: cmd, data: data }))
+      const [result] = await sock.receive()
+      console.log(decode(result))
+      asyncReadline()
+    })
+  }
+  asyncReadline()
 }
 
 run()
