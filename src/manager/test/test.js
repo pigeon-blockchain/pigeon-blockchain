1bac32fe63cf6f0caa3ac46a620f754bd94a3ddf
Joseph C Wang <joe@pigeonchain.co>
2022-01-24 09:37:02 +0800

    add more tests

--- src/manager/test/test.js
+++ src/manager/test/test.js
@@ -1,22 +1,34 @@
-var assert = require('assert');
+const assert = require('assert')
 const Cli = require('../cli')
 const FlockManager = require('../app')
 
-describe('Manager', function() {
-  let cli, app;
-  before(async function() {
+describe('Manager', function () {
+  let cli, app
+  before(async function () {
     app = new FlockManager.FlockManager('tcp://127.0.0.1:3000')
     cli = new Cli.Cli('tcp://127.0.0.1:3000')
     app.run()
   })
-  after(function() {
+  after(function () {
     app.shutdown()
   })
 
-  describe('hello world', function() {
-    it('return hello world', async function() {
+  describe('echo', function () {
+    it('echo', async function () {
       const r = await cli.send('echo hello world')
-      assert.equal(r, 'hello world');
-    });
-  });
-});
+      assert.equal(r, 'hello world')
+    })
+    it('list', async function () {
+      const r = await cli.send('list')
+      assert.ok(r.includes('localhost/pigeon-beacon'))
+    })
+    it('run', async function () {
+      const r = await cli.send('run localhost/pigeon-beacon')
+      assert.ok(r.match(/[0-9a-f]+/))
+    })
+    it('ps', async function () {
+      const r = await cli.send('ps')
+      assert.ok(r.includes('localhost/pigeon-beacon'))
+    })
+  })
+})
