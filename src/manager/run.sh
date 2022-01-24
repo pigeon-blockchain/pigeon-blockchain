b67a6ef8960a4ecd1d3e35b44ba63b6fccd9917f
Joseph C Wang <joe@pigeonchain.co>
2022-01-24 10:36:09 +0800

    run flock-manager

--- src/manager/run.sh
+++ src/manager/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR >> /dev/null
-./app.js &
+./flock-manager.js &
 ./cli.js
 popd >> /dev/null
 pkill -P $$
