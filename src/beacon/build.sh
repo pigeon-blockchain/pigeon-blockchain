b58853360da6d2849b2be4ba58212aaaa2270b46
Joseph C Wang <joe@pigeonchain.co>
2022-01-25 17:19:11 +0800

    remove extra run build

--- src/beacon/build.sh
+++ src/beacon/build.sh
@@ -4,7 +4,6 @@ set -o errexit
 pushd /opt/$1
 dnf install -y --setopt=install_weak_deps=False tar gzip gcc make nodejs npm git
 npm install
-npm run build
 dnf remove -y tar gcc make git
 dnf clean all
 popd
