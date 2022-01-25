bf9bdba04f16f77f1ba3c29417441d238f2a5b4c
Joseph C Wang <joe@pigeonchain.co>
2022-01-26 07:47:39 +0800

    use full port

--- src/beacon/run.sh
+++ src/beacon/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR >> /dev/null
-./app.js >> server.log
+./app.js tcp://0.0.0.0:3000 >> server.log
 popd >> /dev/null
