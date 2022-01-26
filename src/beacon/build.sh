e6fee8f1fa59a901458497371b697cea3e658d55
Joseph C Wang <joe@pigeonchain.co>
2022-01-26 10:21:48 +0800

    save store

--- src/beacon/build.sh
+++ src/beacon/build.sh
@@ -2,8 +2,11 @@
 set -o errexit
 
 pushd /opt/$1
+mkdir -p /data/pigeon-beacon/store
 dnf install -y --setopt=install_weak_deps=False tar gzip gcc make nodejs npm git
 npm install
+rm -rf store
+ln -s ../../data/pigeon-beacon/store store
 dnf remove -y tar gcc make git
 dnf clean all
 popd
--- src/beacon/run.sh
+++ src/beacon/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR >> /dev/null
+mkdir -p /data/pigeon-beacon/store
 ./app.js tcp://0.0.0.0:3000 >> server.log
 popd >> /dev/null
