32f387235bf5886fbbf34d6a7ca3081164c6bcd2
Joseph C Wang <joe@pigeonchain.co>
2022-01-27 12:45:23 +0800

    add flock start and sync sdk

--- bin/flock-start.js
+++ bin/flock-start.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 
 const assert = require('assert')
-const FlockCli = require('pigeon-sdk/js/flock-cli')
-const FlockManager = require('../src/manager/flock-manager')
+const { FlockCli } = require('../src/pigeon-sdk/js/flock-cli')
+const { FlockManager } = require('../src/manager/flock-manager')
 let cli, app
 
-app = new FlockManager.FlockManager('tcp://127.0.0.1:3000')
-cli = new FlockCli.FlockCli('tcp://127.0.0.1:3000')
+app = new FlockManager('tcp://127.0.0.1:3000')
+cli = new FlockCli()
 app.run()
 
 async function main() {
+  await cli.portConnect('default', 'tcp://127.0.0.1:3000')
   const beacon = await cli.send('run localhost/pigeon-beacon')
-  console.log(beacon)
-  const port = await cli.send('port ' + beacon)
-  console.log(port[0][1])
+  let port = await cli.send('port ' + beacon)
+  port = port[0][1].split(':')[1]
+  await cli.portConnect('beacon', 'tcp://127.0.0.1:' + port)
   cli.readline()
 }
 
