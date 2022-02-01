9d07d51348eea49adf0a685d98651ca27fe12604
Joseph C Wang <joe@pigeonchain.co>
2022-02-01 15:50:46 +0800

    checkpoint

--- scripts/flock-start.ts
+++ scripts/flock-start.ts
@@ -7,14 +7,20 @@ const app = new FlockManager({ conport: 'tcp://127.0.0.1:3000' })
 const cli = new FlockCli()
 app.run()
 
+async function runConnect(image: string, connect: string) {
+  const p = await cli.send(`run ${image}`)
+  console.log('starting ${image}')
+  console.log('  ${p}')
+  const portConnectString = await cli.send(`port-connect-string ${p}`)
+  await cli.portConnect(connect, portConnectString[0])
+  return p
+}
+
 async function main () {
   await cli.portConnect('default', 'tcp://127.0.0.1:3000')
-  let p = await cli.send('run localhost/pigeon-beacon')
-  let portConnectString = await cli.send(`port-connect-string ${p}`)
-  await cli.portConnect('beacon', portConnectString[0])
-  p = await cli.send('run localhost/js-algebra')
-  portConnectString = await cli.send(`port-connect-string ${p}`)
-  await cli.portConnect('js-algebra', portConnectString[0])
+  await runConnect('localhost/pigeon-beacon', 'beacon')
+  await runConnect('localhost/js-algebra', 'js-algebra')
+  await runConnect('localhost/flock-monitor', 'flock-monitor')
   cli.readline()
 }
 
