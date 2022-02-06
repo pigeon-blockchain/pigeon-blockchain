52ef016b75030ded2a11aa4929e126973c2d89fc
Joseph C Wang <joequant@gmail.com>
2022-02-06 14:50:13 +0800

    refactor build.sh

--- src/beacon/build.sh
+++ src/beacon/build.sh
@@ -3,14 +3,7 @@ set -o errexit
 
 pushd /opt/$1
 mkdir -p /data/$1/store
-npm install
-chmod a+x *.js *.sh
 rm -rf store
 ln -s ../../data/$1/store store
-rm /etc/dnf/protected.d/*
-npm prune --production
-npm cache clean --force
-dnf autoremove -y tar gcc make git npm shadow-utils sudo
-dnf clean all
 popd
 
