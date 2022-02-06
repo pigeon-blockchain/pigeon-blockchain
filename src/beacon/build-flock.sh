af0b10a1ba50568a10425f2502acf95b8c30db1c
Joseph C Wang <joequant@gmail.com>
2022-02-06 12:09:07 +0800

    reorganize beacon

--- src/beacon/build-flock.sh
+++ src/beacon/build-flock.sh
@@ -1,26 +1,7 @@
 #!/usr/bin/env bash
-
-set -o errexit
-
-# Create a container
-container=$(buildah from localhost/flock-base-js)
-mountpoint=$(buildah mount $container)
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 name=columba-beacon
+maintainer="Joseph C Wang <joequant@gmail.com>"
+base=localhost/flock-base-js
 
-# Labels are part of the "buildah config" command
-buildah config --label maintainer="Joseph C Wang <joe@columbachain.co>" $container
-
-cp $script_dir/*.sh $mountpoint/tmp
-chmod a+x $mountpoint/tmp/*.sh
-mkdir -p $mountpoint/opt
-cp -r $script_dir/$name $mountpoint/opt
-mv $mountpoint/tmp/run.sh $mountpoint/opt/$name
-chmod a+x $mountpoint/opt/$name
-buildah run $container /tmp/build.sh $name
-
-# Entrypoint, too, is a “buildah config” command
-buildah config --cmd /opt/$name/run.sh $container
-
-# Finally saves the running container to an image
-buildah commit --squash --format docker $container $name:latest
+. $script_dir/build-flock-body.sh
--- src/beacon/build.sh
+++ src/beacon/build.sh
@@ -2,10 +2,11 @@
 set -o errexit
 
 pushd /opt/$1
-mkdir -p /data/columba-beacon/store
+mkdir -p /data/$1/store
 npm install
+chmod a+x *.js *.sh
 rm -rf store
-ln -s ../../data/columba-beacon/store store
+ln -s ../../data/$1/store store
 rm /etc/dnf/protected.d/*
 npm prune --production
 npm cache clean --force
