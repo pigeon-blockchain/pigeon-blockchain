defc64e6be251ede00afdb9565cd29df8b30957f
Joseph C Wang <joequant@gmail.com>
2022-02-06 22:35:55 +0800

    add run.sh

---
+++ src/flocks/examples/js/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+exec $script_dir/app.js --conport tcp://0.0.0.0:3000 --pubport tcp://0.0.0.0:3001 --beaconprefix tcp://host.containers.internal
